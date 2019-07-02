30ee1b557231abe4e6cca32404e2675f9195d1a7
syntacsugar <48946999+syntacsugar@users.noreply.github.com>
2019-07-02 10:56:30 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ htpasswd -c -b /etc/nginx/.htpasswd $SYSLOG_USERNAME $SYSLOG_PASSWORD
 cd /var/www
 php7.2 -f create-user.php
 chown www-data:www-data config.auth.user.php
-rm -f create-user.php
+#rm -f create-user.php
 cd
 service php7.2-fpm start
 service supervisor start
